bc617f01f6c680f5ee9af59a84177d0f22c6d4f2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-08-29 13:46:24 -0300

    Add mapping for telescope + lsp references (`<leader>pr`)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,2 +1,9 @@
 -- TODO: Make it work
 -- vim.diagnostic.config({ virtual_text = false })
+
+-- TODO: Learn: https://www.lunarvim.org/docs/beginners-guide/keybinds-overview#lsp
+
+lvim.builtin.which_key.mappings['pr'] = {
+  "<cmd>lua require('telescope.builtin').lsp_references()<cr>",
+  "find references"
+}
