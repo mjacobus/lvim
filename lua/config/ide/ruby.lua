b035df860ee57b8a1a612881aafe1b44fd0b0428
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-11-15 17:00:47 -0300

    Play with ruby lsp

--- lua/config/ide/ruby.lua
+++ lua/config/ide/ruby.lua
@@ -2,6 +2,18 @@
 
 local M = {}
 
+
+-- https://github.com/neovim/nvim-lspconfig/issues/1886#issuecomment-1201356197
+local solargraph_cmd = function()
+  local ret_code = nil
+  local jid = vim.fn.jobstart("bundle info solargraph", { on_exit = function(_, data) ret_code = data end })
+  vim.fn.jobwait({ jid }, 5000)
+  if ret_code == 0 then
+    return { "bundle", "exec", "solargraph", "stdio" }
+  end
+  return { "solargraph", "stdio" }
+end
+
 _timers = {}
 local function setup_diagnostics(client, buffer)
   if require("vim.lsp.diagnostic")._enable then
@@ -64,13 +76,16 @@ M.setup = function()
   }
 
   local lsp_manager = require("lvim.lsp.manager")
-  -- lsp_manager.setup("solargraph")
-  lsp_manager.setup("ruby_ls", {
-    cmd = { "bundle", "exec", "ruby-lsp" },
-    on_attach = function(client, buffer)
-      setup_diagnostics(client, buffer)
-    end,
-  })
+    lsp_manager.setup("solargraph", {
+      cmd = solargraph_cmd()
+    })
+  -- lsp_manager.setup("ruby_ls", {
+  --   cmd = { "bundle", "exec", "ruby-lsp" },
+  --   on_attach = function(client, buffer)
+  --     setup_diagnostics(client, buffer)
+  --   end,
+  -- })
 end
+
 
 return M
