065c31ff2ae6005919a3e4e34f62ed887e2680f1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-01-05 10:19:03 -0300

    Update zenmode width

--- after/plugin/zenmode.lua
+++ after/plugin/zenmode.lua
@@ -2,7 +2,7 @@ require("user.utils").safe_require("zen-mode", function(zen)
   vim.keymap.set("n", "<leader>zz", function()
     zen.setup {
       window = {
-        width = 120,
+        width = 140,
         options = {}
       },
     }
